bb0d38f9d2fc36c3277ff60a3755f4c4337c1506
bonnyped <bonnyped@student.21-school.ru>
2023-09-19 14:49:57 +0600

    minor changes

--- src/05/part5.sh
+++ src/05/part5.sh
@@ -6,5 +6,5 @@ TOTAL_FOLDERS=$(find ${1} -mindepth 1 -type d | wc -l)
 
 echo -en "Total number of folders (including all nested ones) = ${TOTAL_FOLDERS} \n\n"
 echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
-top_five_folders 5
+top_five_folders $1 5
 echo -en "${TOP_FIVE_FOLDERS}\n"
